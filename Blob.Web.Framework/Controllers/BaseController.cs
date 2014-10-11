b38c57180b18d10f4c23879026e3bdfecb1ff5b9
romkap <romeshkap@gmail.com>
2014-10-11 18:58:17 +0530

    Controllers
    Added controllers

---
+++ Blob.Web.Framework/Controllers/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace Blob.Web.Framework.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+
+        public virtual string RenderPartialViewToString()
+        {
+            return RenderPartialViewToString(null, null);
+        }
+
+        public virtual string RenderPartialViewToString(object model)
+        {
+            return RenderPartialViewToString(null, model);
+        }
+
+        public virtual string RenderPartialViewToString(string viewName, object model)
+        {
+
+            //if (string.IsNullOrEmpty(viewName))
+            //    viewName = this.ControllerContext.RouteData.GetRequiredString("action");
+
+            this.ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = System.Web.Mvc.ViewEngines.Engines.FindPartialView(this.ControllerContext, viewName);
+                var viewContext = new ViewContext(this.ControllerContext, viewResult.View, this.ViewData, this.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        //additional logging releated methods should be added here.
+    }
+}
---
+++ Blob.Web.Framework/Mvc/BaseBlobModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blob.Web.Framework.Mvc
+{
+     public partial class BaseBlobModel
+    {
+
+         public BaseBlobModel()
+        {
+            this.CustomProperties = new Dictionary<string, object>();
+        }
+
+         /// <summary>
+         /// Use this property to store any custom value for your models. 
+         /// </summary>
+         public Dictionary<string, object> CustomProperties { get; set; }
+    }
+
+     /// <summary>
+     /// Base Blob entity model
+     /// </summary>
+     public partial class BaseBlobEntityModel : BaseBlobModel
+     {
+         public virtual int Id { get; set; }
+     }
+}
